46326db0ad9742aab068a17ad7fd0411abc2dbb3
ksvlearning <ksvlearning@gmail.com>
2023-07-18 12:27:52 +0530

    18-7-23 Changing commit

--- bmsjavascript.js
+++ bmsjavascript.js
@@ -1,5 +1,4 @@
 // siginbutton
-
 document.getElementById("signinbtn").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "flex";
   document.querySelector("body").style.overflow = "hidden";
@@ -11,7 +10,6 @@ document.getElementById("closebtn").addEventListener("click", function () {
 });
 
 // menubar
-
 document.getElementById("menubtn").addEventListener("click", function () {
   document.querySelector(".menucontainer").style.right = "0";
   document.querySelector("body").style.overflow = "hidden";
@@ -22,17 +20,40 @@ document.querySelector(".menucontainer1").addEventListener("click", function () 
   document.querySelector("body").style.overflow = "auto";
 });
 
+// Menulogin
+document.getElementById("menulogin").addEventListener("click", function () {
+  document.querySelector(".menucontainer").style.right = "-100%";
+  document.querySelector(".popup").style.display = "flex";
+  document.querySelector("body").style.overflow = "hidden";
+
+});
 
 // Section1 sliderbutton
-
 document.getElementById("s1rightarrow").addEventListener("click", function () {
   document.querySelector(".section1").style.left = "-100%";
   document.getElementById("s1rightarrow").style.display = "none";
   document.getElementById("s1leftarrow").style.display = "flex";
+  document.querySelector(".s1second").style.display = "flex";
 });
 
 document.getElementById("s1leftarrow").addEventListener("click", function () {
-  document.querySelector(".section1").style.left = "0";
+  document.querySelector(".section1").style.left = "0%";
   document.getElementById("s1rightarrow").style.display = "flex";
   document.getElementById("s1leftarrow").style.display = "none";
+  document.querySelector(".s1second").style.display = "none";
+});
+
+// section3 sliderbutton
+document.getElementById("s3rightarrow").addEventListener("click", function () {
+  document.getElementById("section3").style.left = "-100%";
+  document.getElementById("s3rightarrow").style.display = "none";
+  document.getElementById("s3leftarrow").style.display = "flex";
+  document.getElementById("s3second").style.display = "flex";
+});
+
+document.getElementById("s3leftarrow").addEventListener("click", function () {
+  document.getElementById("section3").style.left = "0%";
+  document.getElementById("s3rightarrow").style.display = "flex";
+  document.getElementById("s3leftarrow").style.display = "none";
+  document.getElementById("s3second").style.display = "none";
 });
